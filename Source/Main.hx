a3c9c1b83a0ea708ba33b82a8a989db67b78054d
Harry French <harry.french@moonforge.com>
2021-10-06 00:17:16 +0100

    order corrected

--- Source/Main.hx
+++ Source/Main.hx
@@ -12,15 +12,15 @@ class Main extends Sprite
 		var cont = new LayoutGroup();
 		addChild(cont);
 
-		cont.addChild(new Button(0xff0000,"7"));
-		cont.addChild(new Button(0xff0000,"8"));
-		cont.addChild(new Button(0xff0000,"9"));
-		cont.addChild(new Button(0xff0000,"4"));
-		cont.addChild(new Button(0xff0000,"5"));
-		cont.addChild(new Button(0xff0000,"6"));
 		cont.addChild(new Button(0xff0000,"1"));
 		cont.addChild(new Button(0xff0000,"2"));
 		cont.addChild(new Button(0xff0000,"3"));
+		cont.addChild(new Button(0xff0000,"4"));
+		cont.addChild(new Button(0xff0000,"5"));
+		cont.addChild(new Button(0xff0000,"6"));
+		cont.addChild(new Button(0xff0000,"7"));
+		cont.addChild(new Button(0xff0000,"8"));
+		cont.addChild(new Button(0xff0000,"9"));
 		cont.addChild(new Button(0xff0000,"C"));
 		cont.addChild(new Button(0xff0000,"0"));
 		cont.addChild(new Button(0xff0000,"="));
